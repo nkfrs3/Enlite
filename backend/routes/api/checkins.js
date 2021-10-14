a17898f0a37ebe8060e12c2a45c965ef560f2441
nick <nyfrase3@live.com>
2021-10-14 19:31:19 -0400

    successful cheeckin feature

--- backend/routes/api/checkins.js
+++ backend/routes/api/checkins.js
@@ -1,14 +1,21 @@
 const express = require('express')
 const asyncHandler = require('express-async-handler');
+const {Checkin} =require('../../db/models')
 const router = express.Router();
 
-// router.get()
+// router.get('/', asyncHandler)
 
 
-// router.put('/', asyncHandler (async(req, res) => {
+router.put('/', asyncHandler (async(req, res) => {
+  const {userId, shopId} = req.body;
 
-
-// }))
+  const user = await Checkin.create({
+    userId,
+    shopId
+  });
+  console.log(user);
+  return res.json(user);
+}))
 
 
 
--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,14 +2,14 @@ import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import session from '../store/session'
 import shops from '../store/shops'
-import checkIns from './checkins';
+// import checkIns from './checkins';
 import reviews from '../store/reviews'
 
 const rootReducer = combineReducers({
   session,
   shops,
   reviews,
-  checkIns
+  // checkIns
 });
 
 let enhancer;
