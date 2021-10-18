e635d229c848b06889d5427465d6a482a12398ff
nick <nyfrase3@live.com>
2021-10-17 22:53:09 -0400

    fixed footer alignment on profile page

--- frontend/src/components/LandingPage/LandingPage.js
+++ frontend/src/components/LandingPage/LandingPage.js
@@ -1,20 +1,13 @@
-import { useSelector } from 'react-redux';
-import { useState } from 'react';
+
 import './LandingPage.css'
 
 const LandingPage = () => {
-  const currentUser = useSelector(state => state.session.user);
-  const [showModal, setShowModal] = useState();
-  const handleClick = (e) => {
-    e.preventDefault();
 
-  }
  return (
    <>
   <div className='landing-page'>
     <h1>Enlite</h1>
     <h3>A place to discover and share local coffee spots in Ohio.</h3>
-   {!currentUser  && <button onClick={handleClick}>Get Started</button>}
   </div>
 
   </>
